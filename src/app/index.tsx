1c9dc96cd2cbb1325e8bd5291fbce5b86f9a1fe0
Danilo da Silva Siqueira <ssiqueira.danilo@gmail.com>
2024-12-12 22:22:55 -0300

    chore: criação do componente de botão.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,18 @@
+import { View } from "react-native"
+
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 import { Welcome } from "@/components/welcome"
-import { View, Text } from "react-native"
 
 export default function Index() {
     return (
         <View style={{ flex: 1, padding: 40, gap: 40 }}>
             <Welcome />
             <Steps />
+
+            <Button>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
